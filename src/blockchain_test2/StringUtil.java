b27768f220dc204555dbba56374ec6bab025d29f
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2018-10-16 17:30:05 +0000

    StringUtil.java edited online with Bitbucket

--- src/blockchain_test2/StringUtil.java
+++ src/blockchain_test2/StringUtil.java
@@ -26,7 +26,6 @@ import java.util.Base64;
  */
 public class StringUtil {
     
-    //Applies ECDSA Signature and returns the result ( as bytes ).
 	public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
 		Signature dsa;
                 Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
@@ -44,7 +43,6 @@ public class StringUtil {
 	return output;
 	}
 	
-	//Verifies a String signature 
 	public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
 		try {
 			Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
@@ -59,24 +57,13 @@ public class StringUtil {
 	public static String getStringFromKey(Key key) {
 		return Base64.getEncoder().encodeToString(key.getEncoded());
         }
-        
-        /*public static PublicKey getKeyFromString(String s) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchProviderException {
-            // NE DELAA ?
-            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-            byte[] decodedKey = Base64.getDecoder().decode(s);
-            KeyFactory factory = KeyFactory.getInstance("ECDSA", "BC");
-            PublicKey ecPublicKey = (ECPublicKey) factory.generatePublic(new X509EncodedKeySpec(decodedKey));
-            return ecPublicKey;
-        }*/
 
     public static String applySha256(String input){
         try {
 			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-	        
-			//Applies sha256 to our input, 
 			byte[] hash = digest.digest(input.getBytes("UTF-8"));
 	        
-			StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
+			StringBuffer hexString = new StringBuffer();
 			for (int i = 0; i < hash.length; i++) {
 				String hex = Integer.toHexString(0xff & hash[i]);
 				if(hex.length() == 1) hexString.append('0');
